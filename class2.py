4bac1b7904f3c57e2459c8aa7dcb395a04e8ec34
Buckc95 <cb92845@gmail.com>
2024-10-29 12:40:57 -0500

    Clases introducción
    Operadores

---
+++ class2.py
@@ -0,0 +1,15 @@
+#Ejercicio1
+
+number1 = float(input("ingrese el primer numero: "))
+number2 = float(input("Ingrese el segundo numero: "))
+
+suma = number1 + number2
+resta = number1 - number2
+multiplicacion = number1 * number2
+division = number1 / number2
+
+print(f"suma = {suma}")
+print(f"resta = {resta}")
+print(f"multiplicacion = {multiplicacion}")
+print(f"division = {division}")
+
