3d38148a3136e24d3acd8ca22fe2b4fa85ed16d5
Buckc95 <cb92845@gmail.com>
2026-07-17 03:02:35 -0500

    review first class

--- Review/Review_one.py
+++ Review/Review_one.py
@@ -1,10 +1,10 @@
-num_1 = float(input("ingrese el primer numero:"))
-num_2 = float(input("ingrese el segundo numero:"))
+numb_1 = float(input("ingrese el primer numero:"))
+numb_2 = float(input("ingrese el segundo numero:"))
 
-suma = num_1 + num_2
-resta = num_1 - num_2
-multiplicacion = num_1 * num_2
-division = num_1 / num_2
+suma = numb_1 + numb_2
+resta = numb_1 - numb_2
+multiplicacion = numb_1 * numb_2
+division = numb_1 / numb_2
 
 print(f"suma: {suma}")
 print(f"resta: {resta}")
