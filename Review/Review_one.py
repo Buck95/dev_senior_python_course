f7ce5ccfd3278cb7e4303b31da11d4bda606b092
Buckc95 <cb92845@gmail.com>
2026-07-17 02:55:03 -0500

    Subiendo estructura completa del curso y carpeta Review

---
+++ Review/Review_one.py
@@ -0,0 +1,13 @@
+num_1 = float(input("ingrese el primer numero:"))
+num_2 = float(input("ingrese el segundo numero:"))
+
+suma = num_1 + num_2
+resta = num_1 - num_2
+multiplicacion = num_1 * num_2
+division = num_1 / num_2
+
+print(f"suma: {suma}")
+print(f"resta: {resta}")
+print(f"multiplicacion: {multiplicacion}")
+print(f"division: {division}") 
+
