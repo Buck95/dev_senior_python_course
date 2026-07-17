ba6950469ca84d8f9adffdf9982a95a2bf4d8018
Buckc95 <cb92845@gmail.com>
2026-07-17 03:05:32 -0500

    Review first class

--- Review/Review_one.py
+++ Review/Review_one.py
@@ -3,11 +3,11 @@ numb_2 = float(input("ingrese el segundo numero:"))
 
 suma = numb_1 + numb_2
 resta = numb_1 - numb_2
-multiplicacion = numb_1 * numb_2
+multiplicación = numb_1 * numb_2
 division = numb_1 / numb_2
 
 print(f"suma: {suma}")
 print(f"resta: {resta}")
-print(f"multiplicacion: {multiplicacion}")
+print(f"multiplicación: {multiplicación}")
 print(f"division: {division}") 
 
